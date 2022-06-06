868d7c896e028fbc0c0b1674d9fef03929964cd5
dragons96 <521274311@qq.com>
2022-06-06 14:28:08 +0800

    update: 版本升级至0.0.4

--- setup.py
+++ setup.py
@@ -4,15 +4,12 @@ except:
     from distutils.core import setup
 import setuptools
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='sp_tools',
     author='dragons',
     version='0.0.4',
     description='为简化Python开发而生',
-    long_description=long_description,
+    long_description='Python基础开发工具包',
     author_email='521274311@qq.com',
     url='https://gitee.com/kingons/sp_tools',
     packages=setuptools.find_packages(),
